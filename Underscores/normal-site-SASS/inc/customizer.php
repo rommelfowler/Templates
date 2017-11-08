565cdefa9b404cb43f591a83ff6763d9f5db5cc2
rommel santiago <rommelsantiago@rommels-Mac-Pro.local>
2017-11-08 10:17:25 -0800

    WP Templates initial

---
+++ Underscores/normal-site-SASS/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Website Theme Theme Customizer
+ *
+ * @package Website_Theme
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function normal_site_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'normal_site_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function normal_site_customize_preview_js() {
+	wp_enqueue_script( 'normal_site_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'normal_site_customize_preview_js' );
